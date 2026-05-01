df65b912c3819989293f52dadc87f4c90d0b3fa0
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2026-05-01 12:54:29 +0300

    Add @vercel/analytics package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-/*import { Analytics } from "@vercel/analytics/next";*/
+import { Analytics } from "@vercel/analytics/next";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/header";
 import "./globals.css";
